66e0f6946e8de1890309da9ca11bc24b60742704
Aman1kr <ama.kumar@altudo.co>
2023-05-11 15:44:41 +0530

    MCK-POC pending components ui

---
+++ src/rendering/src/components/RecipeButton/RecipeButton.tsx
@@ -0,0 +1,24 @@
+import {
+  Link as JssLink,
+  LinkField
+} from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+import Styles from './RecipeButton.module.scss';
+
+type RecipeButtonProps = ComponentProps & {
+  fields: {
+    ButtonText: Text;
+    ButtonLink: LinkField;
+    ButtonColor: Text;
+  };
+};
+const recipeButton = (props: RecipeButtonProps): JSX.Element => {
+    return (
+      <div className = {Styles.owBtn}>
+      <JssLink field={props.fields.ButtonLink}/>
+      </div>
+    );
+    };
+
+    export const Default = recipeButton;
+
---
+++ src/rendering/src/components/RelatedRecipes/RelatedRecipes.tsx
@@ -0,0 +1,64 @@
+import { Field, Image, ImageField, LinkField } from '@sitecore-jss/sitecore-jss-nextjs';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { ComponentProps } from 'lib/component-props';
+import Link from 'next/link';
+import styles from './RelatedRecipes.module.scss';
+
+type RelatedRecipesProps = ComponentProps & {
+  fields: {
+    items: {
+      fields: {
+        Title: Field<string>;
+        Link: LinkField;
+        Image: ImageField;
+      };
+    }[];
+  };
+};
+
+const RelatedRecipes = (props: RelatedRecipesProps): JSX.Element => {
+  // console.log(props.fields.items[0].fields.Title.value);
+
+  return (
+    <div className="">
+      <div className="row">
+        <div className="col">
+          <div className="RelatedRecipese_Title">
+            <h2>Related Recipes</h2>
+          </div>
+          <div className={`RelatedRecipese_cardContainer  d-flex ${styles.cardbody}`}>
+            {props.fields.items.map((item) => (
+              <div className={`card ${styles.card} rounded-0`}>
+                <span className="d-none">
+                  {
+                    (item.fields.Image.value.src = item.fields.Image.value.src.replace(
+                      'http://cm',
+                      ''
+                    ))
+                  }
+                </span>
+                <Image field={item.fields.Image} className="card-img-top rounded-0" alt="Card image cap" />
+                <div className="card-body">
+                  <Link
+                    className={`card-text text-decoration-none ${styles.title}`}
+                    href={item.fields.Link.value}
+                  >
+                    {item.fields.Title.value}
+                  </Link>
+                </div>
+                <div className={`d-flex justify-content-end ${styles.surround} mt-5 `}>
+                  <i className="bi bi-calendar"></i>
+                  <i className="bi bi-heart"></i>
+                  <i className="bi bi-share"></i>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RelatedRecipes;
