12548bc908034ceac5ad7f1a0a1c8f3aec9cdacd
asmitaparkar <asmita.parkar@altudo.co>
2023-06-09 15:17:43 +0530

    Revert "main menu"
    This reverts commit b2d239755325c61c8022fda32491b51ed963c48e.

---
+++ src/rendering/src/components/Heading/Heading.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text, Field } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+type HeadingProps = ComponentProps & {
+  fields: {
+    Heading: Field<string>;
+  };
+};
+
+const Heading = (props: HeadingProps): JSX.Element => {
+  return (
+    <>
+      <div className="page-content">
+        <div className="container">
+          <section className="mt-10 pt-4 border-container">
+            <h2 className="title title-center">
+              <Text field={props.fields.Heading} />
+            </h2>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default React.memo(Heading);
