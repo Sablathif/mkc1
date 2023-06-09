b734f42ca7fcf3b737239e35ac79f9e4beb96958
EDYNAMIC\NawazM <nawaz.mehdi@altudo.co>
2023-06-09 14:32:28 +0530

    updated

--- src/rendering/src/components/FEaaSWrapper.tsx
+++ src/rendering/src/components/FEaaSWrapper.tsx
@@ -1,13 +1,14 @@
+import { FEaaSComponent, FEaaSComponentProps } from '@sitecore-jss/sitecore-jss-nextjs';
 import React from 'react';
-import FEaaSComponent from './FEaaSComponent';
 
-export const Default = (): JSX.Element => {
-  const styles = `component feaas `.trimEnd();
+export const Default = (props: FEaaSComponentProps): JSX.Element => {
+  const styles = `component feaas ${props.params?.styles}`.trimEnd();
+  const id = props.params?.RenderingIdentifier;
 
   return (
-    <div className={styles}>
+    <div className={styles} id={id ? id : undefined}>
       <div className="component-content">
-        <FEaaSComponent params={undefined} fields={undefined} />
+        <FEaaSComponent {...props} />
       </div>
     </div>
   );
