0959dcd8253907d0226eac7a4ec6caa2a908e5cb
EDYNAMIC\NawazM <nawaz.mehdi@altudo.co>
2023-06-16 18:11:04 +0530

    updated

--- src/rendering/src/components/StatsCard/StatsCard.tsx
+++ src/rendering/src/components/StatsCard/StatsCard.tsx
@@ -5,31 +5,32 @@ import StatsCardType from './StatsCard.type';
 import StatsCardData from './StatsCardData';
 
 const StatsCard = (props: StatsCardType): JSX.Element => (
-  //   <Reveal keyframes={ fadeIn } delay="50" duration="1000" triggerOnce>
-  <Reveal keyframes={fadeIn} delay={50} duration={1000} triggerOnce>
-    <section className="about-section pb-10">
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col-lg-4 mb-10 mb-lg-4">
-            <h5 className="section-subtitle lh-2 ls-md font-weight-normal">
-              {props?.fields?.Title?.value}
-            </h5>
-            <h3 className="section-title lh-1 font-weight-bold">
-              {props?.fields?.SubTitle?.value}
-            </h3>
-            <RichText className="section-desc" field={props?.fields?.Description} />
-          </div>
-          <div className="col-lg-8 ">
-            <div className="row">
-              {props?.fields?.StatsItems?.map((data, index) => (
-                <StatsCardData props={data} index={index} key={index} />
-              ))}
+  <div className="page-content mt-10 pt-10">
+    <Reveal keyframes={fadeIn} delay={50} duration={1000} triggerOnce>
+      <section className="about-section pb-10">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-4 mb-10 mb-lg-4">
+              <h5 className="section-subtitle lh-2 ls-md font-weight-normal">
+                {props?.fields?.Title?.value}
+              </h5>
+              <h3 className="section-title lh-1 font-weight-bold">
+                {props?.fields?.SubTitle?.value}
+              </h3>
+              <RichText className="section-desc" field={props?.fields?.Description} />
+            </div>
+            <div className="col-lg-8 ">
+              <div className="row">
+                {props?.fields?.StatsItems?.map((data, index) => (
+                  <StatsCardData props={data} index={index} key={index} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </Reveal>
+      </section>
+    </Reveal>
+  </div>
 );
 
 export default StatsCard;
