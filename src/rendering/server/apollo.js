a1dbfdfa6a6fb8cb1abd44da264ef23db29fc60f
asmitaparkar <asmita.parkar@altudo.co>
2023-06-16 15:12:11 +0530

    modified appolo js

--- src/rendering/server/apollo.js
+++ src/rendering/server/apollo.js
@@ -19,8 +19,13 @@ const API_KEY = '1047AEE5-9BCD-4DBF-9744-A26E12B79AB6';
 
 const client = new ApolloClient({
   // uri: `https://cm.xmcloudcm.localhost/sitecore/api/graph/edge?sc_apikey=${API_KEY}`,
-  uri: `https://xmc-4d5bao4pu1oqtch7xvzyao.sitecorecloud.io/sitecore/api/graph/edge?sc_apikey=${API_KEY}`,
+  uri: `https://xmc-3xp0ocausxnwn8mk5pncth.sitecorecloud.io/sitecore/api/graph/edge?sc_apikey=${API_KEY}`,
   cache: new InMemoryCache(),
+  headers: {
+    fetchOptions: {
+      mode: 'no-cors',
+    },
+  },
 });
 
 export default client;
